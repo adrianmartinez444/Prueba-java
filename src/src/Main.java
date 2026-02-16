4ac1e7759388f1124effe1d851442930ff277965
adrianmartinez444 <adrian.martinez.rey.0@gmail.com>
2026-02-16 09:04:36 -0500

    Practica de print

---
+++ src/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String nombre = "Adrian";
+        int edad = 17;
+        double salario = 1500.00;
+        boolean activo = true;
+
+
+        System.out.println ("Nombre: " + nombre);
+        System.out.println ("Edad: " + edad);
+        System.out.println ("Salario: " + salario);
+        System.out.println ("Activo: " + activo);
+
+    }
+}
